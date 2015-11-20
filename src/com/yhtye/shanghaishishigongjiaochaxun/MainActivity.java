a390619be93b47a8685d485e3db550321d29f9c1
xiongqr <xiongqr@ucweb.com>
2015-11-20 11:08:39 +0800

    增加主页导航栏

--- src/com/yhtye/shanghaishishigongjiaochaxun/MainActivity.java
+++ src/com/yhtye/shanghaishishigongjiaochaxun/MainActivity.java
@@ -7,7 +7,9 @@ import com.yhtye.shgongjiao.tools.RegularUtil;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,12 +19,55 @@ public class MainActivity extends Activity {
     
     private Intent intent=new Intent(); 
     
+    private Button shishichaxunButton = null;
+    private Button huanshengchaxunButton = null;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        init();
+    }
+    
+    /**
+     * 初始化按钮和界面元素
+     */
+    private void init() {
+        shishichaxunButton = (Button) findViewById(R.id.shishichaxun);
+        huanshengchaxunButton = (Button) findViewById(R.id.huanshengchaxun);
         // 获取输入信息
         numberoneEditText = (EditText)findViewById(R.id.numberone);
+        
+        // 初始化
+        shishichaxunButton.setSelected(true);
+        huanshengchaxunButton.setSelected(false);
+    }
+    
+    /**
+     * 实时查询按钮
+     * 
+     * @param v
+     */
+    public void shishichaxunClick(View v) {
+        // 按钮变化
+        shishichaxunButton.setTextColor(getResources().getColor(R.color.blue));
+        shishichaxunButton.setSelected(true);
+        huanshengchaxunButton.setTextColor(getResources().getColor(R.color.white));
+        huanshengchaxunButton.setSelected(false);
+    }
+    
+    /**
+     * 换乘查询按钮
+     * 
+     * @param v
+     */
+    public void huanshengchaxunClick(View v) {
+        // 按钮变化
+        shishichaxunButton.setTextColor(getResources().getColor(R.color.white));
+        shishichaxunButton.setSelected(false);
+        huanshengchaxunButton.setTextColor(getResources().getColor(R.color.blue));
+        huanshengchaxunButton.setSelected(true);
     }
     
     /**
@@ -37,7 +82,7 @@ public class MainActivity extends Activity {
             return;
         }
         String lineName = numberoneEditText.getText().toString();
-        if (lineName == null || lineName == "") {
+        if (TextUtils.isEmpty(lineName)) {
             Toast.makeText(MainActivity.this, "请输入要查询的公交路线", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -47,7 +92,6 @@ public class MainActivity extends Activity {
         }
         
         // 切换Activity
-//        Intent intent=new Intent(); 
         intent.setClass(MainActivity.this, ResultActivity.class);  
         intent.putExtra("lineName", lineName);
         startActivity(intent);
