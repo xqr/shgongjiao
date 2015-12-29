b0fccf6ca44ce9685d4005471ee47df0fac59a39
xiongqr <xiongqr@ucweb.com>
2015-12-29 19:54:48 +0800

    fix null

--- src/com/yhtye/shanghaishishigongjiaochaxun/ResultActivity.java
+++ src/com/yhtye/shanghaishishigongjiaochaxun/ResultActivity.java
@@ -158,6 +158,9 @@ public class ResultActivity extends BaseActivity implements OnItemClickListener 
         @Override  
         public void handleMessage(Message msg) {  
             final ResultActivity  theActivity =  mActivity.get();
+            if (theActivity == null) {
+                return;
+            }
             int messageFlag = msg.what;
             if (messageFlag == LineMessage) {
                 // 线路信息
